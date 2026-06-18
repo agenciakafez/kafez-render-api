e016ba8ab3f35d142280e2bf454f958ba0d0c6f0
agenciakafez <boblymarketing@gmail.com>
2026-06-18 17:00:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
